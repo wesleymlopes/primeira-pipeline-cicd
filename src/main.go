58a9e0ab3d0cf425f031372aca0068d5dc1512cb
wesleymlopes <54637237+wesleymlopes@users.noreply.github.com>
2026-09-22 17:39:43 -0300

    V2.0

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 func index(w http.ResponseWriter, r *http.Request) {
 
 	w.Header().Set("Content-Type", "text/plain")
-	fmt.Fprintf(w, "Aplicacao exemplo 4.0")
+	fmt.Fprintf(w, "Aplicacao exemplo - v2.0")
 }
 
 func main() {
